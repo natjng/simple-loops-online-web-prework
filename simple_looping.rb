8a07e62f6835e1cbb42bbd144b56b12397c56555
Natalie J <natsetc@gmail.com>
2019-05-05 02:23:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -40,7 +40,7 @@ def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   until number_of_times == 0
     puts phrase
-    number_of_times -= 0
+    number_of_times -= 1
   end
 end
 
